38001bfb32d90c608033f8be1d4d3ea1633ba2af
80LK <80lkru@gmail.com>
2020-11-09 23:13:26 +0300

    Update rotate constants

--- devlib/structure_const.js
+++ devlib/structure_const.js
@@ -1,58 +1,76 @@
-Structure.ROTATE_NONE = [
+Structure.ROTATE_NONE = new Rotate([
 	1,0,0,
 	0,1,0,
 	0,0,1
-];
+]);
 	
-Structure.ROTATE_90Y = [
+Structure.ROTATE_90Y = new Rotate([
 	0,0,-1,
 	0,1,0,
 	1,0,0
-];
-Structure.ROTATE_180Y = [
+]);
+Structure.ROTATE_180Y = new Rotate([
 	-1,0,0,
 	0,1,0,
 	0,0,-1
-];
-Structure.ROTATE_270Y = [
+]);
+Structure.ROTATE_270Y = new Rotate([
 	0,0,1,
 	0,1,0,
 	-1,0,0
-];
+]);
 	
-Structure.ROTATE_90X = [
+Structure.ROTATE_90X = new Rotate([
 	1,0,0,
 	0,0,-1,
 	0,1,0
-];
-Structure.ROTATE_180X = [
+]);
+Structure.ROTATE_180X = new Rotate([
 	1,0,0,
 	0,-1,0,
 	0,0,-1
-];
-Structure.ROTATE_270X = [
+]);
+Structure.ROTATE_270X = new Rotate([
 	1,0,0,
 	0,0,1,
 	0,-1,0
-];
+]);
 	
-Structure.ROTATE_90Z = [
+Structure.ROTATE_90Z = new Rotate([
 	0,-1,0,
 	1,0,0,
 	0,0,1
-];
-Structure.ROTATE_180Z = [
+]);
+Structure.ROTATE_180Z = new Rotate([
 	-1,0,0,
 	0,-1,0,
 	0,0,1
-];
-Structure.ROTATE_270Z = [
+]);
+Structure.ROTATE_270Z = new Rotate([
 	0,1,0,
 	-1,0,0,
 	0,0,1
-];
+]);
 
-Structure.ROTATE_ALL = Structure.ROTATE_RANDOM = [
+Structure.MIRROR_X = new Rotate([
+	-1,0,0,
+	0,1,0,
+	0,0,1
+]);
+Structure.MIRROR_Y = new Rotate([
+	1,0,0,
+	0,-1,0,
+	0,0,1
+]);
+Structure.MIRROR_Z = new Rotate([
+	1,0,0,
+	0,1,0,
+	0,0,-1
+]);
+
+
+Structure.ROTATE_ALL =
+Structure.ROTATE_RANDOM = [
 	Structure.ROTATE_180X,
 	Structure.ROTATE_180Y,
 	Structure.ROTATE_180Z,
@@ -69,20 +87,4 @@ Structure.ROTATE_Y = [
 	Structure.ROTATE_90Y,
 	Structure.ROTATE_180Y,
 	Structure.ROTATE_270Y
-];
-
-Structure.MIRROR_X = [
-	-1,0,0,
-	0,1,0,
-	0,0,1
-];
-Structure.MIRROR_Y = [
-	1,0,0,
-	0,-1,0,
-	0,0,1
-];
-Structure.MIRROR_Z = [
-	1,0,0,
-	0,1,0,
-	0,0,-1
 ];
